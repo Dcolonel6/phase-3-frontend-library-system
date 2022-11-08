7987a8326e3bbb1474ff09bb741a86633908696c
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-11-08 22:22:10 +0300

    added browser router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
 import NavBar from './Components/NavBar';
+import Login from './Components/Login';
+import BooksContainer from './Components/BooksContainer'
+import BooksDetails from './Components/BooksDetails';
 
 function App() {
   return (
     <div className="container container-fluid">
-      <header className="App-header">
-        <NavBar />        
-      </header>
+      <BrowserRouter>
+      <div className="">
+        <NavBar />
+        <Switch>
+          <Route path="/" exact>
+            <Login/>
+          </Route>
+          <Route path="/books/" exact>
+            <BooksContainer />
+          </Route> 
+          <Route path="/books/:id" exact>
+            <BooksDetails/>
+          </Route>           
+        </Switch>
+      </div>
+    </BrowserRouter>
     </div>
   );
 }
